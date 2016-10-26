b4c4208498a2dcf4361a223a94c6fd7105a88032
Zach Benton <Zach@Zach.local>
2016-10-26 17:40:31 -0500

    Options request for CORS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -56,4 +56,13 @@ object Application extends Controller {
     }.getOrElse(BadRequest("invalid json"))
   }
 
+  def preflight = {
+    Ok().withHeaders(
+      "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With",
+      "Access-Control-Allow-Credentials" -> "true",
+      "Access-Control-Max-Age" -> (60 * 60 * 24).toString
+    )
+  }
 }
