317a836774e74ede5dd80e84db3693d1d97caf50
Zach Benton <Zach@Zach.local>
2016-10-26 17:58:21 -0500

    Make options request an action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -56,13 +56,13 @@ object Application extends Controller {
     }.getOrElse(BadRequest("invalid json"))
   }
 
-  def preflight(all: String) = {
-    Ok(Json.obj("status" -> "ok")).withHeaders(
+  def preflight(all: String) = Action({
+    Ok(Json.obj()).withHeaders(
       "Access-Control-Allow-Origin" -> "*",
       "Access-Control-Allow-Methods" -> "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With",
       "Access-Control-Allow-Credentials" -> "true",
       "Access-Control-Max-Age" -> (60 * 60 * 24).toString
     )
-  }
+  })
 }
